12dca5352b567d6ecacac111e09a22b4496b2110
Mainak Deb <mainakdeb1998@gmail.com>
2024-03-06 12:51:27 +0530

    data_insertion

--- django_data_CRUD_operation/project/app/urls.py
+++ django_data_CRUD_operation/project/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.index,name="home"),
     path('registered',views.success,name="success"),
+    path('insert',views.insert,name="insert")
 ]
--- django_data_CRUD_operation/project/app/views.py
+++ django_data_CRUD_operation/project/app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from app.models import Userdetails
+from app.forms import UserInfo
 
 # Create your views here.
 
@@ -8,3 +10,46 @@ def index(request):
 
 def success(request):
     return render(request, "success.html")
+
+
+############____ CRUD for ADMIN USERINFO _____#############
+
+
+def insert(request):
+    if request.method == "POST":
+        form = UserInfo(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/')
+
+            except:
+                pass
+    else:
+        form = UserInfo()
+    return render(request, 'success.html', {'form': form})
+
+
+def show(request):
+    userdetails = Userdetails.objects.all()
+    return render(request, "admin/userinfo.html", {'userdetails': userdetails})
+
+
+def edit(request, id):
+    userinfo = Userdetails.objects.get(id=id)
+    return render(request, 'admin/edit.html', {'userinfo': userinfo})
+
+
+def update(request, id):
+    userinfo = Userdetails.objects.get(id=id)
+    form = UserInfo(request.POST, instance=userinfo)
+    if form.is_valid():
+        form.save()
+        return redirect("/show")
+    return render(request, 'admin/edit.html', {'userinfo': userinfo})
+
+
+def destroy(request, id):
+    userinfo = Userdetails.objects.get(id=id)
+    userinfo.delete()
+    return redirect("/show")
